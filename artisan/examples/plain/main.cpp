610732336a50c69aedf77696bb4a095b382839f4
wojciech <32068559+clima-dev@users.noreply.github.com>
2022-01-28 09:37:27 +0100

    Create main.cpp

---
+++ artisan/examples/plain/main.cpp
@@ -0,0 +1,25 @@
+#include "../include/Artisan.h"
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
+
+int main(void)
+{
+    artisan::Window win(SCREEN_WIDTH,SCREEN_HEIGHT,"Artisan | Flappy Bird");
+
+    win.setBgColor(artisan::RGBColor{ 244, 98, 54 });
+
+    while (!win.closed())
+    {  
+        // game loop
+        
+        begin_drawing();
+            
+            clear_bg(win.getBgColor()); 
+
+            // rendering
+
+        end_drawing(win);
+    }
+
+    return 0;
+}
